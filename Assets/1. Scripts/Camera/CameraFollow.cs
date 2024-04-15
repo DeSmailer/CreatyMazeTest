5214a9f086a109b2888d08538fa8a852ceabbbe8
Pyliavskyi Dmytro <dmytro.pyliavskyi@nure.ua>
2024-04-15 14:24:39 +0300

    update CameraFollow

--- Assets/1. Scripts/Camera/CameraFollow.cs
+++ Assets/1. Scripts/Camera/CameraFollow.cs
@@ -3,18 +3,30 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     [SerializeField] private Transform _target;
+    [SerializeField] private float _smoothSpeed = 0.125f;
 
-    private Vector3 initalOffset;
-    private Vector3 cameraPosition;
+    private Vector3 _locationOffset;
+    private Vector3 _rotationOffset;
 
     private void Start()
     {
-        initalOffset = transform.position - _target.position;
+        _locationOffset = transform.position;
+        _rotationOffset = transform.rotation.eulerAngles;
     }
 
-    private void LateUpdate()
+    void FixedUpdate()
     {
-        cameraPosition = _target.position + initalOffset;
-        transform.position = cameraPosition;
+        Follow();
+    }
+
+    private void Follow()
+    {
+        Vector3 desiredPosition = _target.position + _target.rotation * _locationOffset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, _smoothSpeed);
+        transform.position = smoothedPosition;
+
+        Quaternion desiredrotation = _target.rotation * Quaternion.Euler(_rotationOffset);
+        Quaternion smoothedrotation = Quaternion.Lerp(transform.rotation, desiredrotation, _smoothSpeed);
+        transform.rotation = smoothedrotation;
     }
 }
