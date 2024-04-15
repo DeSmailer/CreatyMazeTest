54d653fae133aeb16ec93534c2ced3655dc742cd
Pyliavskyi Dmytro <dmytro.pyliavskyi@nure.ua>
2024-04-15 12:19:25 +0300

    added FenceDoor animations

--- Assets/1. Scripts/Maze/Door/Door.cs
+++ Assets/1. Scripts/Maze/Door/Door.cs
@@ -1,21 +1,24 @@
-using BrokenVector.LowPolyFencePack;
 using UnityEngine;
 
 public class Door : MonoBehaviour
 {
-    [SerializeField] private DoorController _doorController;
+    [SerializeField] private Animator _animator;
+    private const string OPEN_ANIMATION = "open";
+    //[SerializeField] private DoorController _doorController;
     [SerializeField] private WinZone _winZone;
 
     public WinZone WinZone => _winZone;
 
     public void Initialize(KeyManager keyManager)
     {
-        _doorController.CloseDoor();
+        //_doorController.CloseDoor();
         keyManager.OnAllKeysPicked.AddListener(Open);
     }
 
     public void Open()
     {
-        _doorController.OpenDoor();
+        //_doorController.OpenDoor();
+        _animator.Play(OPEN_ANIMATION);
     }
+
 }
