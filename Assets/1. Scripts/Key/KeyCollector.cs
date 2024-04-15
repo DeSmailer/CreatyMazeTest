1d3b30db565e5e9133dc65af6e4a49841645e63c
Pyliavskyi Dmytro <dmytro.pyliavskyi@nure.ua>
2024-04-15 13:51:18 +0300

    added sounds

--- Assets/1. Scripts/Key/KeyCollector.cs
+++ Assets/1. Scripts/Key/KeyCollector.cs
@@ -2,11 +2,14 @@ using UnityEngine;
 
 public class KeyCollector : MonoBehaviour
 {
+    [SerializeField] private AudioSource _audioSource;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.GetComponent<ICollectable>() is ICollectable collectable)
         {
             collectable.Collect();
+            _audioSource.Play();
         }
     }
 }
--- Assets/1. Scripts/Maze/Door/Door.cs
+++ Assets/1. Scripts/Maze/Door/Door.cs
@@ -4,21 +4,21 @@ public class Door : MonoBehaviour
 {
     [SerializeField] private Animator _animator;
     private const string OPEN_ANIMATION = "open";
-    //[SerializeField] private DoorController _doorController;
+
+    [SerializeField] private AudioSource _audioSource;
+
     [SerializeField] private WinZone _winZone;
 
     public WinZone WinZone => _winZone;
 
     public void Initialize(KeyManager keyManager)
     {
-        //_doorController.CloseDoor();
         keyManager.OnAllKeysPicked.AddListener(Open);
     }
 
     public void Open()
     {
-        //_doorController.OpenDoor();
         _animator.Play(OPEN_ANIMATION);
+        _audioSource.Play();
     }
-
 }
